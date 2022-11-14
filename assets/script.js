11b950baedbf19b4e8a611002563dfa4ac18e2f2
YasinDehfuli <Ydehfuli@Gmail.Com>
2022-11-14 23:29:40 +0330

    add animation to parts fix some bugs and add new button for form

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,41 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+var $btn = $("button.btnSubmit");
+var $loaderTemplate = $("svg.loader");
+var $checkmarkTemplate = $("svg.checkmark");
+
+$btn.on('click', function() {
+    if ($(this).hasClass('clicked')) return;
+    $(this).addClass('clicked');
+    var self = $(this);
+    var timeout1 = 600,
+        timeout2 = 2500,
+        timeout3 = 3000;
+    setTimeout(function() {
+
+        self.append($loaderTemplate.clone());
+        self.find('svg').removeClass('svg--template');
+        self.find('svg').css('display', 'initial');
+    }, timeout1);
+    setTimeout(function() {
+        self.text('');
+        self.find('svg').remove();
+        self.append($checkmarkTemplate.clone());
+        self.find('svg').css('display', 'initial');
+        self.find('svg').removeClass('svg--template');
+        self.addClass('done');
+    }, timeout1 + timeout2);
+    setTimeout(function() {
+        self.find('svg').remove();
+        self.text('Submit');
+        self.removeClass('clicked');
+        self.removeClass('done');
+    }, timeout1 + timeout2 + timeout3);
+});
+
+$(function () {
+    $("#contactForm").click(function (e) {
+        e.preventDefault()
+    })
+})
